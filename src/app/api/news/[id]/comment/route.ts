49a65521c15b1956feee0c6a86b5ea1518164a66
Chetan Sharma <chetansharma.meta@gmail.com>
2024-06-09 16:29:22 +0530

    Error solved -3

--- src/app/api/news/[id]/comment/route.ts
+++ src/app/api/news/[id]/comment/route.ts
@@ -2,9 +2,9 @@ import dbConnect from "@/lib/dbConnect";
 import Comment from "@/model/Comment";
 import User from "@/model/User";
 import Article from "@/model/Article";
-import { NextRequest } from "next/server";
+import { NextRequest, Response } from "next/server";
 
-export async function POST(req: NextRequest) {
+export async function POST(req: NextRequest): Promise<void | Response> {
   await dbConnect();
   try {
     const formData = await req.formData();
@@ -41,28 +41,7 @@ export async function POST(req: NextRequest) {
     });
   } catch (error) {
     console.log("Error", error);
-    return {
-      status: 500,
-      body: { message: "Error in creating comment" },
-    };
-  }
-}
-
-export async function GET(
-  req: NextRequest,
-  { params }: { params: { id: string } }
-) {
-  await dbConnect();
-  const { id } = params;
-  try {
-    const comments = await Comment.find({ article: id }).populate("user");
-    return new Response(JSON.stringify(comments), {
-      status: 200,
-      headers: { "Content-Type": "application/json" },
-    });
-  } catch (error) {
-    console.log("Error", error);
-    return new Response(JSON.stringify({ message: "Internal server error" }), {
+    return new Response(JSON.stringify({ message: "Error in creating comment" }), {
       status: 500,
       headers: { "Content-Type": "application/json" },
     });
