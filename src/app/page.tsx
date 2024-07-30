bd9e0b471baf9d47f15db38fe224414df63aa6b1
chetansharma-meta <chetansharma.meta@gmail.com>
2024-07-30 18:45:40 +0530

    testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ import { InfiniteNews } from "@/components/InfiniteNews";
 
 export default function Home() {
   const { data: session } = useSession();
-
   const router = useRouter();
   console.log(session?.user);
   return (
